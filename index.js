5ca86f21240f868c0783033a583957b7845a00d2
Noya Antman Ron <noya@niio.com>
2022-10-10 15:04:31 +0300

    design changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 contactButton = document.querySelector("#contact button");
 
 contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
+  contactButton.style.color = "red";
 });
